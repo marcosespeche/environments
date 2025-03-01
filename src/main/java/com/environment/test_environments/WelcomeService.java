97be6d82ed2976e5c345b1fd3974dd75169f4a78
marcosespeche <marcos.espeche.28@gmail.com>
2025-03-01 14:46:51 -0300

    "Changing sout for logging"

--- src/main/java/com/environment/test_environments/WelcomeService.java
+++ src/main/java/com/environment/test_environments/WelcomeService.java
@@ -1,22 +1,27 @@
 package com.environment.test_environments;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Data
+@Slf4j
 public class WelcomeService {
 
+  private static final Logger log = LoggerFactory.getLogger(WelcomeService.class);
   @Value("${environment.name}")
   private String environmentName;
 
   public String getWelcome() throws InterruptedException {
 
-    System.out.println("Loading... \n");
+    log.info("Loading... \n");
     Thread.sleep(3000);
 
-    System.out.println(environmentName);
+    log.info("ENVIRONMENT NAME: {}", environmentName);
 
     return "Hello, world!";
   }
