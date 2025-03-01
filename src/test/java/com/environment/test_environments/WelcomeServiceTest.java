55c3eab6191c06dba29702942d4d811c26fd30d6
marcosespeche <marcos.espeche.28@gmail.com>
2025-03-01 16:36:09 -0300

    Fixing unit testing

--- src/test/java/com/environment/test_environments/WelcomeServiceTest.java
+++ src/test/java/com/environment/test_environments/WelcomeServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -11,10 +12,12 @@ class WelcomeServiceTest {
 
   @Autowired private WelcomeService service;
 
+  @Value("${environment.name}") private String environmentName;
+
   @Test
   void getWelcome() throws InterruptedException {
 
-    String expected = "Hello, world!";
+    String expected = "Hello, you are currently in a " + environmentName + " environment!";
 
     String actual = service.getWelcome();
 
